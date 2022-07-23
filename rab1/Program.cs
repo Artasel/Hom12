93999ca2919a6b66d3fbfa30c2bffb9965250c97
Artasel <aleksejbobkin163@gmail.com>
2022-07-23 20:39:52 +0200

    домашка

---
+++ rab1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите M");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumInterval(int a, int b)
+{
+    int result = 0;
+    if (a == b + 1) return result;
+    return result += a + SumInterval(a + 1, b);
+}
+
+Console.WriteLine(SumInterval(M, N));
+
+
---
+++ rab2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 29
+*/
+
+Console.WriteLine("Введите M");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Akkerman(int number, int argument)
+{
+    if (number == 0) return argument + 1;
+    else if(argument == 0)  return Akkerman(number - 1, 1);
+    else return Akkerman(number - 1, Akkerman(number, argument - 1));
+
+}
+
+Console.WriteLine(Akkerman(M, N));
